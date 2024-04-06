d118ba89f83503234b698eacf9a61ac83f6bef05
satya <satya159@gmail.com>
2024-04-06 12:15:39 +0530

    leetcode example for buyand sell and remove duplicates for array

--- src/main/java/com/datastructures/linkedlist/TestLinkedList.java
+++ src/main/java/com/datastructures/linkedlist/TestLinkedList.java
@@ -5,6 +5,8 @@ public class TestLinkedList {
     TestLinkedList testLinkedList=new TestLinkedList();
     testLinkedList.createSingleLinkedList(5);
         System.out.println(testLinkedList.head.value);
+        testLinkedList.insertLinkedList(6,0);
+        System.out.println(testLinkedList.head.value);
     }
 
     public Node head;
@@ -22,4 +24,35 @@ public class TestLinkedList {
         return head;
     }
 
+    //Insert
+
+
+    public void insertLinkedList(int nodeVale,int location) {
+       Node node=new Node();
+       node.value=nodeVale;
+       if(head==null){
+           createSingleLinkedList(nodeVale); //O(1)
+           return;
+       }else if(location ==0){
+           node.next=head;//O(1)
+           head=node;//O(1)
+       }else if (location>=size){//O(1)
+           node.next=null;//O(1)
+           tail.next=node;//O(1)
+           tail=node;//O(1)
+       }
+       else{
+           Node tempNode=head;
+           int index =0;
+           while(index<location-1){//O(N)
+               tempNode=tempNode.next;
+               index++;
+           }
+           Node nextNode=tempNode.next;
+           tempNode.next=node;
+           node.next=nextNode;
+
+       }
+       size++;
+    }
 }
